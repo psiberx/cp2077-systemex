33514c1ebd03b537985522da206b430e9937eb16
pv <p@siberx.dev>
2023-07-15 22:51:23 +0300

    Add redscript hints

--- tools/dist/install-mod.ps1
+++ tools/dist/install-mod.ps1
@@ -1,9 +1,8 @@
-param ($GameDir, $ReleaseBin, $ProjectName = "SystemEx")
+param ($GameDir, $ProjectName = "SystemEx")
 
 $StageDir = "build/package"
 $Version = & $($PSScriptRoot + "\steps\get-version.ps1")
 
-& $($PSScriptRoot + "\steps\compose-archives.ps1") -StageDir ${StageDir} -ProjectName ${ProjectName}
 & $($PSScriptRoot + "\steps\compose-redscripts.ps1") -StageDir ${StageDir} -ProjectName ${ProjectName} -Version ${Version}
 & $($PSScriptRoot + "\steps\compose-hints.ps1") -StageDir ${StageDir}
 & $($PSScriptRoot + "\steps\install-from-stage.ps1") -StageDir ${StageDir} -GameDir ${GameDir}
